f766814d4e496bc2656f2871691777f77f9134bf
Aishwarya S R <Aishwarya_S_R@mckinsey.com>
2022-04-22 21:01:06 +0530

    fix: update Students fields

--- models/students.js
+++ models/students.js
@@ -1,24 +1,18 @@
-'use strict';
 const {
-  Model
+  Model,
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Students extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate() {
     }
   }
   Students.init({
+    rollNo: DataTypes.INTEGER,
     name: DataTypes.STRING,
-    roll_no: DataTypes.INTEGER,
-    class: DataTypes.STRING,
+    class: DataTypes.INTEGER,
     section: DataTypes.STRING,
-    marks: DataTypes.INTEGER
+    totalMarks: DataTypes.FLOAT,
   }, {
     sequelize,
     modelName: 'Students',
