2ca0c3f1674362a785152c321ed8fb079b4bbfc5
Aishwarya S R <Aishwarya_S_R@mckinsey.com>
2022-04-25 08:51:21 +0530

    feat: add fetch students by class and section field

---
+++ src/index.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-console */
+const express = require('express');
+const env = require('dotenv');
+const { graphqlHTTP } = require('express-graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLInt,
+  GraphQLString,
+} = require('graphql');
+const dbOperations = require('./services/dbOperations.service');
+
+env.config();
+const port = process.env.PORT || 8000;
+const app = express();
+
+const StudentType = new GraphQLObjectType({
+  name: 'Student',
+  description: 'This reprsents a student',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    rollNo: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    class: { type: new GraphQLNonNull(GraphQLString) },
+    section: { type: new GraphQLNonNull(GraphQLString) },
+    totalMarks: { type: new GraphQLNonNull(GraphQLString) },
+  }),
+});
+
+const rootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root query',
+  fields: () => ({
+    studentsClass: {
+      type: new GraphQLList(StudentType),
+      description: 'List of all the students filtered based on the class',
+      args: {
+        class: { type: GraphQLInt },
+      },
+      resolve: (parent, args) => dbOperations.getStudentsByClass(args.class),
+    },
+    studentsClassSection: {
+      type: new GraphQLList(StudentType),
+      description: 'List of all the students filtered based on the class and section',
+      args: {
+        class: { type: GraphQLInt },
+        section: { type: GraphQLString },
+      },
+      resolve: (parent, args) => dbOperations.getStudentsByClassSection(args.class, args.section),
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: rootQueryType,
+});
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
---
+++ src/services/dbOperations.service.js
@@ -0,0 +1,28 @@
+const { Students } = require('../../models');
+
+const getStudentsByClass = async (givenClass) => {
+  const students = await Students.findAll({
+    attributes: { exclude: ['createdAt,updatedAt'] },
+    where: {
+      class: givenClass,
+    },
+    order: [['id', 'ASC']],
+  });
+  return students;
+};
+
+const getStudentsByClassSection = async (givenClass, givenSection) => {
+  const students = await Students.findAll({
+    attributes: { exclude: ['createdAt,updatedAt'] },
+    where: {
+      class: givenClass,
+      section: givenSection,
+    },
+    order: [['id', 'ASC']],
+  });
+  return students;
+};
+
+module.exports = {
+  getStudentsByClass, getStudentsByClassSection,
+};
